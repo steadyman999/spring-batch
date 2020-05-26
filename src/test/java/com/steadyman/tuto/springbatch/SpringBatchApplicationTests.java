29575fb0226532622ffe88574c8b511be2918358
steadyman999 <steadyman999@gmail.com>
2020-05-26 11:39:23 +0900

    gradle test script 실행 실패 수정

--- src/test/java/com/steadyman/tuto/springbatch/SpringBatchApplicationTests.java
+++ src/test/java/com/steadyman/tuto/springbatch/SpringBatchApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBatchApplicationTests {
+public class SpringBatchApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }

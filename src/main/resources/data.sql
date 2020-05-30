4fe8270529333702ece7a2194f1ffdf24ad4bac9
steadyman999 <steadyman999@gmail.com>
2020-05-30 23:23:26 +0900

    테스트 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@
 -- insert into PERSON (NAME, SEX, PHONE) values ('ccc', 'male', '010-3333-3333');
 
 insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (0, '제니', 6000000);
--- insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (1, '칸', 8000000);
--- insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (2, '레고', 5000000);
--- insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (3, '하프', 2500000);
--- insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (5, '코코', 4000000);
+insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (1, '칸', 8000000);
+insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (2, '레고', 5000000);
+insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (3, '하프', 2500000);
+insert into EMPLOYEE (DEPARTMENT_ID, NAME, SALARY) values (5, '코코', 4000000);
